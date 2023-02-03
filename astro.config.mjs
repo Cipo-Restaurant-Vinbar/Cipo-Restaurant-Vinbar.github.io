cdfa17d1df47cd684e030e39028743a44c5b86dd
Karl Grønvaldt <kgronvaldt@gmail.com>
2023-02-03 19:23:53 +0100

    prototype 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',
   site: 'https://cipo.dk',
-  integrations: [sitemap()],
+  integrations: [sitemap(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
 });
---
+++ src/data/MenuItems.js
@@ -0,0 +1,75 @@
+export const BarSnacks = [
+  {
+    item: "Marinated Sicilian green olives",
+    price: "50",
+  },
+  {
+    item: "Comté 36 mois",
+    price: "95",
+  },
+  {
+    item: "Baba ganoush with dukkah",
+    price: "75",
+  },
+  {
+    item: "Pork terrine with mustard and pickles",
+    price: "135",
+  },
+  {
+    item: "Pata Negra",
+    price: "130",
+  },
+  {
+    item: "Sourdough from Albatross & venner, unfiltered olive oil",
+    price: "55",
+  },
+]
+
+export const ALaCarte = [
+  {
+    item: "Grilled cauliflower with browned butter and hazelnuts",
+    price: "125",
+  },
+  {
+    item: "Salt-baked celeriac with walnuts, salted lemon, and apple",
+    price: "120",
+  },
+  {
+    item: "Grilled carrots with goat cheese, pine nuts, and balsamico",
+    price: "135",
+  },
+  {
+    item: "Charred leeks with lumpfish roe, vinaigrette, and herbs",
+    price: "145",
+  },
+  {
+    item: "Scallops with cucumber consommé and kohlrabi",
+    price: "145",
+  },
+  {
+    item: "Beef tartare with pickled beech mushroom and koji mayo",
+    price: "145",
+  },
+  {
+    item: "Coq au vin",
+    price: "245",
+  }
+]
+
+export const Cheese = [
+  {
+    item: "3 kinds of danish cheese, hip rose, and rye",
+    price: "145",
+  }
+]
+
+export const Dessert = [
+  {
+    item: "Tiramisu with pistaccio",
+    price: "120",
+  },
+  {
+    item: "Affogato",
+    price: "85",
+  }
+]
